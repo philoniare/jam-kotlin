cd213797376d32b3801c23c5bc783bcd7d5bc5f4
philoniare <philoniare@gmail.com>
2024-10-30 18:16:06 +0800

    feat: add erasure coding test cases

---
+++ jam-core/src/test/kotlin/io/forge/jam/core/EcData.kt
@@ -0,0 +1,13 @@
+package io.forge.jam.core
+
+import io.forge.jam.core.serializers.ByteArrayHexSerializer
+import io.forge.jam.core.serializers.ByteArrayListHexSerializer
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class EcData(
+    @Serializable(with = ByteArrayHexSerializer::class)
+    val data: ByteArray,
+    @Serializable(with = ByteArrayListHexSerializer::class)
+    val chunks: List<ByteArray>
+)
---
+++ jam-core/src/test/kotlin/io/forge/jam/core/ErasureCodingTest.kt
@@ -0,0 +1,23 @@
+package io.forge.jam.core
+
+import io.forge.jam.core.encoding.TestFileLoader
+import kotlin.test.Test
+
+class ErasureCodingTest {
+    @Test
+    fun testSimpleEcData() {
+        val folderName = "erasure_coding/ec"
+        val testCases = TestFileLoader.getTestFilenamesFromResources(folderName)
+
+        for (testCase in testCases) {
+            println("Current Test Case: $testCase")
+            val inputCase = TestFileLoader.loadJsonData<EcData>(
+                "$folderName/$testCase",
+            )
+
+//            val encodedChunks = JamErasureCoding.encode(inputCase.data)
+//            assertEquals(inputCase.chunks.size, encodedChunks.size)
+//            assertContentEquals(encodedChunks, inputCase.chunks)
+        }
+    }
+}
